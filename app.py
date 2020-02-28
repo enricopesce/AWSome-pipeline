7d040e2fe9d01aea9cd5d2bbc72bbc298664e14b
Enrico Pesce <enrico.pesce@pixartprinting.com>
2020-02-28 15:09:08 +0100

    Fix CDK environment implementation

--- app.py
+++ app.py
@@ -4,17 +4,22 @@ from aws_cdk import core
 from awsomepipeline.application_stack import WebAppStack
 from awsomepipeline.pipeline_stack import PipelineStack
 from awsomepipeline.vpc_stack import VpcStack
+import os
+
+env=core.Environment(
+    account=os.environ["CDK_DEFAULT_ACCOUNT"],
+    region=os.environ["CDK_DEFAULT_REGION"])
 
 app = core.App()
 
-context = app.node.try_get_context("env")
+context = app.node.try_get_context("stack")
 
-stack_vpc = VpcStack(app,"awsome-vpc")
+stack_vpc = VpcStack(app,"awsome-vpc", env=env)
 
 if context == "prd":
-    WebAppStack(app, "awsome-prd", vpc=stack_vpc.vpc)
+    WebAppStack(app, "awsome-prd", vpc=stack_vpc.vpc, env=env)
 elif context == "stg":
-    WebAppStack(app, "awsome-stg", vpc=stack_vpc.vpc, env_level="stg")
+    WebAppStack(app, "awsome-stg", vpc=stack_vpc.vpc, env_level="stg", env=env)
 elif context == "pipeline":
     PipelineStack(
         app,
@@ -22,9 +27,10 @@ elif context == "pipeline":
         git_token_key="my_secret_token",
         github_branch="master",
         github_owner="enricopesce",
-        github_repo="AWSome-pipeline"
+        github_repo="AWSome-pipeline",
+        env=env
     )
 else:
-    print("Please define the tier context: prd | stg | pipeline. es: --context env=pipeline")
+    print("Please define the stack context: prd | stg | pipeline. es: --context stack=pipeline")
 
 app.synth()
