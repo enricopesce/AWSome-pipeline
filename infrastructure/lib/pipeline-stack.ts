eb8133b938700c5dca8fe52aa1a669c2e675792b
Enrico Pesce <enrico.pesce@pixartprinting.com>
2020-08-05 14:58:06 +0200

    test pipelines

--- infrastructure/lib/pipeline-stack.ts
+++ infrastructure/lib/pipeline-stack.ts
@@ -5,102 +5,103 @@ import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions'
 import * as pipelines from '@aws-cdk/pipelines';
 
 export class PipelineStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, gitToken: string, github_owner: string, github_repo: string,
-    github_branch: string, props?: cdk.StackProps) {
-    super(scope, id, props)
+    constructor(scope: cdk.Construct, id: string, gitToken: string, github_owner: string, github_repo: string,
+        github_branch: string, props?: cdk.StackProps) {
+        super(scope, id, props)
 
-    const sourceArtifact = new codepipeline.Artifact();
-    const cloudAssemblyArtifact = new codepipeline.Artifact();
+        const sourceArtifact = new codepipeline.Artifact();
+        const cloudAssemblyArtifact = new codepipeline.Artifact();
 
-    const pipeline = new pipelines.CdkPipeline(this, 'Pipeline', {
-      cloudAssemblyArtifact,
-      sourceAction: new codepipeline_actions.GitHubSourceAction({
-        actionName: 'GitHub_Source',
-        owner: github_owner,
-        repo: github_repo,
-        branch: github_branch,
-        oauthToken: cdk.SecretValue.secretsManager(gitToken),
-        output: sourceArtifact
-      }),
-      synthAction: pipelines.SimpleSynthAction.standardNpmSynth({
-        actionName: 'banana',
-        sourceArtifact,
-        cloudAssemblyArtifact,
-        subdirectory: 'infrastructure',
-        environmentVariables: {
-            'WORKING_BRANCH': {
-                value: github_branch
-            }
-      })
-    });
-
-    /*     const staging_action = new codepipelineActions.CodeBuildAction({
-            actionName: 'Deliver',
-            project: project,
-            input: source_output,
-            outputs: [staging_output],
-            environmentVariables: {
-                'ENV': {
-                    value: 'stg',
-                },
-                'WORKING_BRANCH': {
-                    value: github_branch
+        const pipeline = new pipelines.CdkPipeline(this, 'Pipeline', {
+            cloudAssemblyArtifact,
+            sourceAction: new codepipeline_actions.GitHubSourceAction({
+                actionName: 'GitHub_Source',
+                owner: github_owner,
+                repo: github_repo,
+                branch: github_branch,
+                oauthToken: cdk.SecretValue.secretsManager(gitToken),
+                output: sourceArtifact
+            }),
+            synthAction: pipelines.SimpleSynthAction.standardNpmSynth({
+                actionName: 'banana',
+                sourceArtifact,
+                cloudAssemblyArtifact,
+                subdirectory: 'infrastructure',
+                environmentVariables: {
+                    'WORKING_BRANCH': {
+                        value: github_branch
+                    }
                 }
-            }
-        })
+            }),
+        });
 
-        const manual_approval_action = new codepipelineActions.ManualApprovalAction({
-            actionName: 'Approve'
-        })
-
-        const production_action = new codepipelineActions.CodeBuildAction({
-            actionName: 'Deliver',
-            project: project,
-            input: source_output,
-            outputs: [production_output],
-            environmentVariables: {
-                'ENV': {
-                    value: 'prd',
-                },
-                'WORKING_BRANCH': {
-                    value: github_branch
+        /*     const staging_action = new codepipelineActions.CodeBuildAction({
+                actionName: 'Deliver',
+                project: project,
+                input: source_output,
+                outputs: [staging_output],
+                environmentVariables: {
+                    'ENV': {
+                        value: 'stg',
+                    },
+                    'WORKING_BRANCH': {
+                        value: github_branch
+                    }
                 }
-            }
-        })
-
-        const bucketArtifacts = new AutoDeleteBucket(this, 'artifacts')
-
-        const pipeline = new codepipeline.Pipeline(this, "Pipeline", {
-            artifactBucket: bucketArtifacts
-        })
-
-        pipeline.addStage({
-            stageName: 'Source',
-            actions: [source_action]
-        })
-
-        pipeline.addStage({
-            stageName: 'Staging',
-            actions: [staging_action]
-        })
-
-        pipeline.addStage({
-            stageName: 'Approval',
-            actions: [manual_approval_action]
-        })
-
-        pipeline.addStage({
-            stageName: 'Production',
-            actions: [production_action]
-        })
-
-        new cdk.CfnOutput(this, 'LinkCodePipelinePage', {
-            value: "https://"
-                + this.region
-                + ".console.aws.amazon.com/codesuite/"
-                + "codepipeline/pipelines/"
-                + pipeline.pipelineName
-                + "/view?region=" + this.region
-        }) */
-  }
+            })
+    
+            const manual_approval_action = new codepipelineActions.ManualApprovalAction({
+                actionName: 'Approve'
+            })
+    
+            const production_action = new codepipelineActions.CodeBuildAction({
+                actionName: 'Deliver',
+                project: project,
+                input: source_output,
+                outputs: [production_output],
+                environmentVariables: {
+                    'ENV': {
+                        value: 'prd',
+                    },
+                    'WORKING_BRANCH': {
+                        value: github_branch
+                    }
+                }
+            })
+    
+            const bucketArtifacts = new AutoDeleteBucket(this, 'artifacts')
+    
+            const pipeline = new codepipeline.Pipeline(this, "Pipeline", {
+                artifactBucket: bucketArtifacts
+            })
+    
+            pipeline.addStage({
+                stageName: 'Source',
+                actions: [source_action]
+            })
+    
+            pipeline.addStage({
+                stageName: 'Staging',
+                actions: [staging_action]
+            })
+    
+            pipeline.addStage({
+                stageName: 'Approval',
+                actions: [manual_approval_action]
+            })
+    
+            pipeline.addStage({
+                stageName: 'Production',
+                actions: [production_action]
+            })
+    
+            new cdk.CfnOutput(this, 'LinkCodePipelinePage', {
+                value: "https://"
+                    + this.region
+                    + ".console.aws.amazon.com/codesuite/"
+                    + "codepipeline/pipelines/"
+                    + pipeline.pipelineName
+                    + "/view?region=" + this.region
+            }) */
+    }
 }
