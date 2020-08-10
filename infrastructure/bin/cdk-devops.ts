0874207e6221d0a1843a80afbcf656c41079e4f0
Enrico Pesce <enrico.pesce@pixartprinting.com>
2020-08-10 15:25:25 +0200

    prova

--- infrastructure/bin/cdk-devops.ts
+++ infrastructure/bin/cdk-devops.ts
@@ -6,6 +6,8 @@ import * as codepipeline from '@aws-cdk/aws-codepipeline'
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions'
 import * as pipelines from '@aws-cdk/pipelines'
 import * as s3 from '@aws-cdk/aws-s3'
+import { ENGINE_METHOD_RAND } from 'constants'
+import { CfnOutput } from '@aws-cdk/core'
 
 export interface Config {
     PROJECT_NAME: string
@@ -33,13 +35,27 @@ function name(suffix: string) {
     return config.PROJECT_NAME + "-" + WORKING_BRANCH + "-" + suffix
 }
 
+export class MardaStack extends cdk.Stack {
+    public readonly urlOutput: cdk.CfnOutput;
+	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props)
+        const bucket = new s3.Bucket(this, 'bucket')
+
+		this.urlOutput = new cdk.CfnOutput(this, 'Url', {
+			value: bucket.bucketDomainName
+		  });
+
+        }
+}        
+
+
 export class ApplicationStage extends cdk.Stage {
-    public readonly urlOutput: string
+    public readonly urlOutput: CfnOutput
     constructor(scope: cdk.Construct, id: string, props?: cdk.StageProps) {
         super(scope, id, props)
         //const service = new ApplicationStack(app, name(id), config.VPC_NAME, id, '/', { env: props?.env })
-        const bucket = new s3.Bucket(this, 'bucket')
-        this.urlOutput = bucket.bucketWebsiteDomainName
+        const service = new MardaStack(this, 'merda', { env: props?.env })
+        this.urlOutput = service.urlOutput
     }
 }
 
@@ -81,5 +97,8 @@ export class PipelineStack extends cdk.Stack {
 }
 
 new PipelineStack(app, name('pipeline'), 'my_secret_token', 'enricopesce', 'AWSome-pipeline', WORKING_BRANCH, { env: env })
+
+
+new ApplicationStage(app, 'merda')
 
 app.synth()
