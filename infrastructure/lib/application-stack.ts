c24bf84f56fb9247f66bd8acda6e000cb4e8cf35
Enrico Pesce <enrico.pesce@pixartprinting.com>
2020-08-28 10:04:45 +0200

    from vpc test 3

--- infrastructure/lib/application-stack.ts
+++ infrastructure/lib/application-stack.ts
@@ -15,17 +15,16 @@ export class ApplicationStack extends cdk.Stack {
 		props?: cdk.StackProps) {
 		super(scope, id, props)
 
-		/* 		console.log("OUTPUT2 ")
-				console.log(JSON.stringify(vpc_name))
-				console.log("OUTPUT2 ")
-				console.log(JSON.stringify(props)) */
+		console.log("VPC NAME: ")
+		console.log(JSON.stringify(vpc_name))
+		console.log("PROPS: ")
+		console.log(JSON.stringify(props))
 
 		const vpc = ec2.Vpc.fromLookup(this, "vpc", { vpcName: vpc_name })
 		//const vpc = new ec2.Vpc(this, "vpc", { maxAzs: 2 })
-		/*
-		console.log("OUTPUT2 ")
+
+		console.log("VPC IMPORTED ")
 		console.log(vpc)
-				*/
 		
 		const sg = new ec2.SecurityGroup(this, 'sg', {
 			vpc: vpc
