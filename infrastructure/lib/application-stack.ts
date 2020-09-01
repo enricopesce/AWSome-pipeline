2aec031b7d70952b519603eb40bcba42a514eaa9
Enrico Pesce <enrico.pesce@pixartprinting.com>
2020-09-01 16:06:28 +0200

    issue github

--- infrastructure/lib/application-stack.ts
+++ infrastructure/lib/application-stack.ts
@@ -4,7 +4,6 @@ import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns'
 import * as ec2 from '@aws-cdk/aws-ec2'
 
 export class ApplicationStack extends cdk.Stack {
-	private fargateService: ecs_patterns.ApplicationLoadBalancedFargateService
 	public readonly urlOutput: cdk.CfnOutput;
 
 	constructor(scope: cdk.Construct, id: string, vpc_name: string, props?: cdk.StackProps) {
@@ -16,7 +15,6 @@ export class ApplicationStack extends cdk.Stack {
 		console.log(JSON.stringify(props))
 
 		const vpc = ec2.Vpc.fromLookup(this, "vpc", { vpcName: vpc_name })
-		//const vpc = new ec2.Vpc(this, "vpc", { maxAzs: 2 })
 
 		console.log("VPC IMPORTED ")
 		console.log(vpc)
