28a8c664f126b255dfee0d3362264b11fc8cf3e0
Enrico Pesce <enrico.pesce@pixartprinting.com>
2020-12-29 16:32:43 +0100

    test

--- infrastructure/bin/cdk-devops.ts
+++ infrastructure/bin/cdk-devops.ts
@@ -38,8 +38,7 @@ export class ApplicationStage extends cdk.Stage {
     public readonly urlOutput: cdk.CfnOutput
     constructor(scope: cdk.Construct, id: string, props?: cdk.StageProps) {
         super(scope, id, props)
-        const service = new ApplicationStack(this, id, config.VPC_NAME, props)
-        this.urlOutput = service.urlOutput
+        const service = new ApplicationStack(this, id, config.VPC_NAME, id, '/', props)
     }
 }
 
